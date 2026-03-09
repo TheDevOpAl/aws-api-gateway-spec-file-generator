63e2569fc78df68744e4d9f0283041e1bbf1062e
thedesertfox2 <62604242+thedesertfox2@users.noreply.github.com>
2026-03-09 12:41:25 -0400

    adding granular request validation

--- src/types/AddRouteParams.ts
+++ src/types/AddRouteParams.ts
@@ -1,7 +1,9 @@
 import { HttpMethod } from "./HttpMethod";
+import { RequestValidationEnum } from "./RequestValidators";
 
 export type AddRouteParams = {
     routeName: string;
     method: HttpMethod;
     summary: string;
+    requestValidator?: RequestValidationEnum;
 };
