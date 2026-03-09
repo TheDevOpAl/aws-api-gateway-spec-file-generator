bf93890bcc6f82523643bb3f708754df87e00231
TheDevOpAl <alexander.gautherin@gmail.com>
2026-03-08 21:59:28 -0400

    Adding request validation block for api gateway validation (#9)
    * adding request validation logic, at least the start
    
    * breaking out validation block creation if needed
    
    ---------
    
    Co-authored-by: thedesertfox2 <62604242+thedesertfox2@users.noreply.github.com>

--- src/OpenApiSpec/openApiSpec.ts
+++ src/OpenApiSpec/openApiSpec.ts
@@ -1,26 +1,58 @@
 import { AddRouteParams } from "../types/AddRouteParams";
 import { PathItem } from "../types/PathItem";
+import { RequestValidators } from "../types/RequestValidators";
+import { SpecFileContent } from "../types/SpecFileContent";
 
 export class OpenApiSpec {
     private openApi: string = "3.0.1";
     private paths: Record<string, PathItem> = {};
     private components: any = {};
     private securitySchemes: any = {};
+    private "x-amazon-apigateway-request-validators": RequestValidators = {}
 
-    public getOpenApiSpecContent() {
-        return {
+    private validationBlockCreatedIfNeeded(requestValidationKey: string): void {
+        if (!this["x-amazon-apigateway-request-validators"][requestValidationKey]) {
+            this["x-amazon-apigateway-request-validators"][requestValidationKey] = {
+                validateRequestBody: false,
+                validateRequestParameters: false
+            }
+        }
+    }
+
+
+    public getOpenApiSpecContent(): SpecFileContent {
+
+        const specContent: SpecFileContent = {
             openapi: this.openApi,
             paths: this.paths,
             components: this.components,
             securitySchemes: this.securitySchemes
         }
+
+        if (this["x-amazon-apigateway-request-validators"]) {
+            specContent["x-amazon-apigateway-request-validators"] = this["x-amazon-apigateway-request-validators"];
+        }
+
+        return specContent
     }
 
-    setOpenApiVersion(version: string): void {
+    public setRequestBodyValidation(requestValidationKey: string): void {
+        this.validationBlockCreatedIfNeeded(requestValidationKey);
+
+        this["x-amazon-apigateway-request-validators"][requestValidationKey].validateRequestBody = true;
+    }
+
+    public setRequestParameterValidation(requestValidationKey: string): void {
+        this.validationBlockCreatedIfNeeded(requestValidationKey);
+
+        this["x-amazon-apigateway-request-validators"][requestValidationKey].validateRequestParameters = true;
+    }
+
+    public setOpenApiVersion(version: string): void {
         this.openApi = version;
     }
 
-    addRoute({routeName, method, summary}: AddRouteParams): void {
+    public addRoute({routeName, method, summary}: AddRouteParams): void {
 
         if (this.paths[routeName]?.[method]) {
             throw new Error(`Method ${method} already exists for route ${routeName}`);
