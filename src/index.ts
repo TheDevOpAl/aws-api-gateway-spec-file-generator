15805e4caae66c4b97c5bb1938f5a9af0fa3546b
TheDevOpAl <alexander.gautherin@gmail.com>
2026-03-10 22:33:30 -0400

    Removed enums from use (#19)
    Co-authored-by: thedesertfox2 <62604242+thedesertfox2@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,4 @@ export { OpenApiSpec } from "./OpenApiSpec/openApiSpec";
 export { HttpMethod } from "./types/HttpMethod";
 export { PathItem } from "./types/PathItem";
 export { AddRouteParams } from "./types/AddRouteParams";
-export { RequestValidationEnum } from "./types/RequestValidators";
 export { RequestParameterType } from "./types/RequestParameter";
-export { AuthorizerTypeEnum } from "./types/AwsAuthorizerSheme";
